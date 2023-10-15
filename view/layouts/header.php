d63659e0d8552ead1fd400fd269308884be41320
Fábio Rodrigues <fabiojob2023@gmail.com>
2023-10-15 00:53:32 -0300

    fix: ajuste path header

--- view/layouts/header.php
+++ view/layouts/header.php
@@ -5,27 +5,26 @@
     <meta charset="UTF-8">
     <title>Cadastro de Cliente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="../public/css/style.css">
+    <link rel="stylesheet" href="<?= __DIR__ . '/../public/css/style.css' ?>">
 
 </head>
 
 <body>
-
     <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
-        <a class="ms-4 navbar-brand" href="#"><img src="../public/img/logo.png" alt="logo" width="250px" height="80px"></a>
+        <a class="ms-4 navbar-brand" href="#"><img src="<?= __DIR__ . '/../public/img/logo.png' ?>" alt="logo" width="250px" height="80px"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-3 gap-3">
                 <li class="nav-item active">
-                    <a class="nav-link" href="../../../Cadastream/view/index.php">Início</a>
+                    <a class="nav-link" href="<?= __DIR__ . '/../../../Cadastream/view/index.php' ?>">Início</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../../../Cadastream/view/about.php">Sobre</a>
+                    <a class="nav-link" href="<?= __DIR__ . '/../../../Cadastream/view/about.php' ?>">Sobre</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../../../Cadastream/view/contact.php">Contato</a>
+                    <a class="nav-link" href="<?= __DIR__ . '/../../../Cadastream/view/contact.php' ?>">Contato</a>
                 </li>
             </ul>
         </div>
